0ca3434d8c879a5ba45aae9b68380e7a630e1cdc
mkenane <morkenan@hotmail.com>
2018-04-24 14:44:11 -0400

    first commit

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, cb) {
+      if (Array.isArray(collection)){
+        for (let i = 0; i < collection.length; i++) {
+          cb(collection[i], i, collection)
+        }
+      }
+      else {
+          let keyArray = Object.keys(collection)
+          for (let i = 0; i < keyArray.length; i++) {
+            cb(collection[keyArray[i]], i, collection)
+          }
+        }
+      return collection
     },
 
     map: function() {
@@ -15,7 +26,7 @@ fi = (function() {
     reduce: function() {
 
     },
-    
+
     functions: function() {
 
     },
